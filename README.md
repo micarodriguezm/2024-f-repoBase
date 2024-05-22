50def36cf8581a0ed369e5850eea736e78d20bf8
micarodriguezm <micarodriguezm1@gmail.com>
2024-05-22 03:08:03 -0300

    Ejercicio Terminado

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# ejercicio-alumno
+# ejercicioPdePop
 
-## Integrantes
+Canciones 
 
-- integrante1 (usuario github)
-- integrante2 (usuario github)
-...
+Cada canción cuenta con un título, una duración en minutos y una lista de instrumentos musicales que se utilizarán en la interpretación. 
+Por el momento, se sabe que la banda cuenta con estas canciones en su repertorio: 
+"Pattern Matching": 4 minutos, instrumentos: guitarra, bajo, batería.  
+"Seis dieciocho": 3 minutos, instrumentos: teclado, guitarra.  
+"La vida en Haskell": 5 minutos, sin instrumentos (es versión acapella).
+
+Aceptación 
+Si el título de la canción comienza con “M” su aceptación será 500, si tiene una duración par de minutos será igual a la cantidad de letras de su nombre por diez, si es acapella su aceptación será 10. 
+*Nota: las condiciones están ordenadas por prioridad, es decir, si su título comienza con “M”, aunque su duración sea par su índice será 500. 
+
+Repertorio 
+De la banda se conoce la lista de canciones que tienen en el repertorio. (Se debe tener en cuenta que en cualquier momento pueden agregarse nuevas canciones al mismo). 
+ 
+Se pide: 
+Definir al menos 2 canciones más para la banda. 
+
+PdePop tiene la costumbre de tocar sus canciones por orden alfabético. Dadas dos canciones, determinar cuál viene antes en el repertorio. 
+
+Determinar si una canción es acapella, es decir, no se utilizan instrumentos en su interpretación. 
+
+Averiguar si una canción es aceptada por el público, esto ocurre cuando su índice de aceptación es mayor a 60. 
+
+Dado un instrumento y una canción, determinar si la canción necesita al instrumento para ser interpretada. 
+
+Tocar una canción: esto implica que, si la canción es aceptada por el público, se la toca tal cual es, en caso contrario, se la toca con la duración reducida a la mitad. 
--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,71 @@
 module Library where
 import PdePreludat
+data Cancion = UnaCancion {
+    titulo :: String,
+    duracion :: Number,
+    instrumentos :: [Instrumento]
+} deriving (Show, Eq)
 
-doble :: Number -> Number
-doble numero = numero + numero
+data Instrumento = Guitarra | Bajo | Bateria | Teclado | Saxofon deriving (Show, Eq)
+
+patternMatching :: Cancion
+patternMatching = UnaCancion "Pattern Matching" 4 [Guitarra, Bajo, Bateria]
+
+seisDieciocho :: Cancion
+seisDieciocho = UnaCancion "Seis dieciocho" 3 [Teclado, Guitarra]
+
+laVidaEnHaskell :: Cancion
+laVidaEnHaskell = UnaCancion "La vida en Haskell" 5 []
+
+aceptacion :: Cancion -> Number
+aceptacion cancion 
+    |head (titulo cancion) == 'M' = 500
+    |even (length (titulo cancion)) = length (titulo cancion) *10
+    |esAcapella cancion = 10
+    |otherwise = 0
+
+type Repertorio = [Cancion]
+repertorio :: Repertorio
+repertorio = [patternMatching, seisDieciocho, seisDieciocho, laVidaEnHaskell, elSensei, amorFuncional]
+
+--Definir dos canciones
+
+elSensei :: Cancion
+elSensei = UnaCancion "El Sensei" 3 [Guitarra]
+
+amorFuncional :: Cancion
+amorFuncional = UnaCancion "Amor Funcional" 5 [Bajo, Saxofon]
+
+--Que cancion viene antes
+
+vaAntes :: Cancion -> Cancion -> Cancion
+vaAntes cancion1 cancion2
+    |titulo cancion1 < titulo cancion2 = cancion1
+    |otherwise = cancion2
+
+--Es acapella
+
+esAcapella :: Cancion -> Bool
+esAcapella cancion = null(instrumentos cancion)
+
+--Es aceptada por el publico
+
+esAceptada :: Cancion -> Bool
+esAceptada cancion = aceptacion cancion > 60
+
+--Determinar si la cancion necesita el instrumento
+
+necesitaElInstrumento :: Instrumento -> Cancion -> Bool
+necesitaElInstrumento instrumento cancion = instrumento `elem` instrumentos cancion
+
+--Si es aceptada por el publico, se toca completa, sino se reduce a la mitad el tiempo
+
+tocarUnaCancion :: Cancion -> Number
+tocarUnaCancion cancion 
+    |esAceptada cancion = duracion cancion
+    |otherwise = duracion cancion / 2
+
+
+
+
+
